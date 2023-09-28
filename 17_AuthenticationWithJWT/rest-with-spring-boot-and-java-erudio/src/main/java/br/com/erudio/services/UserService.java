249430b3763f0674ead5c696b8b089f7dd38b598
gabrielsmm <gabrielsmm251@gmail.com>
2023-09-27 21:39:06 -0300

    Implementing Authentication With JWT

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/UserService.java
@@ -0,0 +1,34 @@
+package br.com.erudio.services;
+
+import java.util.logging.Logger;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	private UserRepository repository;
+	
+	private Logger logger = Logger.getLogger(UserService.class.getName());
+	
+	public UserService(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		logger.info("Finding one user by name " + username + "!");
+		var user = repository.findByUsername(username);
+		if (user != null) {
+			return user;
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+	}
+	
+}
