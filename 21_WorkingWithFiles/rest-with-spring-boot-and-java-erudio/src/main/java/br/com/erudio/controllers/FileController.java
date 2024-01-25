9edb7a104acdfc5887117a951f9dc7b8cb685192
gabrielsmm <gabrielsmm251@gmail.com>
2024-01-24 22:11:27 -0300

    Implementing Upload and Download Files

--- 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
@@ -1,8 +1,17 @@
 package br.com.erudio.controllers;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +22,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.erudio.data.vo.v1.UploadFileReponseVO;
 import br.com.erudio.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Tag(name = "File Endpoint")
 @RestController
@@ -35,6 +45,37 @@ public class FileController {
 				.toUriString();
 		
 		return new UploadFileReponseVO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+	}
+	
+	@PostMapping("/uploadMultipleFiles")
+	public List<UploadFileReponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+		logger.info("Storing files to disk");
+		
+		return Arrays.asList(files)
+				.stream()
+				.map(file -> uploadFile(file))
+				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/downloadFile/{fileName:.+}")
+	public ResponseEntity<Resource> downloadFile(@PathVariable("fileName") String fileName, HttpServletRequest request) {
+		logger.info("Reading a file on disk");
+		
+		Resource resource = service.loadFileAsResource(fileName);
+		String contentType = "";
+		
+		try {
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+		} catch (Exception e) {
+			logger.info("Could not determine file type!");
+		}
+		
+		if (contentType.isBlank()) contentType = "application/octet-stream";
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
 	}
 	
 }
